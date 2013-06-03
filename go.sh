899a6fec58c300c6a3675467a82a871558e7b4f1
Dominic Canare <dom@domstyle.net>
2013-06-03 00:28:36 -0500

    add jar creation

--- go.sh
+++ go.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-javac -source 1.5 -target 1.5 -d bin src/org/greenlightgo/teacherattack/*.java
+javac -source 1.6 -target 1.6 -d bin src/org/greenlightgo/teacherattack/*.java
 if [ $? -eq 0 ]; then
 	cd bin
+	jar -cfe TeacherAttack-client.jar org.greenlightgo.teacherattack.PlayerApp org/ resources/ >/dev/null 2>&1
+	cp TeacherAttack-client.jar /tmp >/dev/null 2>&1
 	class=$1
 	shift
 	java "org.greenlightgo.teacherattack.$class" $*
