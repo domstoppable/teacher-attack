4d0b9ba82ab898aae1f6fa6deb92b71688df046f
Dominic Canare <dom@domstyle.net>
2013-09-21 22:55:12 -0500

    add server jar

--- go.sh
+++ go.sh
@@ -4,6 +4,7 @@ javac -source 1.6 -target 1.6 -d bin src/org/greenlightgo/teacherattack/*.java
 if [ $? -eq 0 ]; then
 	cd bin
 	jar -cfe TeacherAttack-client.jar org.greenlightgo.teacherattack.PlayerApp org/ resources/ >/dev/null 2>&1
+	jar -cfe TeacherAttack-server.jar org.greenlightgo.teacherattack.GameServer org/ resources/ >/dev/null 2>&1
 	cp TeacherAttack-client.jar /tmp >/dev/null 2>&1
 	class=$1
 	shift
